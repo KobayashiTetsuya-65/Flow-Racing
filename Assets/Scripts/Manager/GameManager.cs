ab6400abd4042eb7f1844c0463bcdba349d7bc42
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-10-14 23:17:54 +0900

    [add]初期設定のもろもろ

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
